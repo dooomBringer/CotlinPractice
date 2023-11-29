ee679c722820313cb954cbedce3e772433ea9ead
Лёха <uvarov111222@gmail.com>
2023-11-29 23:55:55 +0300

    added some functionality

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,36 @@
 fun main(args: Array<String>) {
+
+    val userList = mutableListOf<User>()
+
     printGreetings()
     val currentUser = registration()
-    permanentInterface(currentUser)
+    userList.add(currentUser)
+    permanentInterface(currentUser, userList)
 
 
 }
 
-
-
-fun permanentInterface(user : User) {
+fun permanentInterface(user : User, userList : List<User>) {
     while (true) {
 
         println("""
            Пожалуйста, выберите одну из опций:
            1. Посмотреть информацию об аккаунте
-           2. Выйти из аккаунта
+           2. Посмотреть обо всех аккаунтах
+           3. Выйти из аккаунта
        """.trimIndent())
 
-
         val userChoice = readln().toInt()
-        userOption(userChoice, user)
+        userOption(userChoice, user, userList)
 
+    }
+}
+
+fun printAllUsers(userList: List<User>) {
+    userList.forEach {
+        it.printAllUserData()
+        println("" +
+                "")
     }
 }
 
@@ -53,17 +63,37 @@ fun readPassword () : String {
     println("Введите ваш пароль:")
     return readln()
 }
-
-fun userOption (choice : Int, user : User) {
+fun userOption (choice : Int, user : User, userList: List<User>) {
     if (choice == 1) {
         while (true) {
-
             user.printAllUserData()
-
-            println("Чтобы выйти, введи 1")
-            if (readln().toInt() == 1) {
-                break
-            }
+            println("" +
+                    "")
+            break
+        }
+    } else if (choice == 2) {
+        while (true) {
+            printAllUsers(userList)
+            println("" +
+                    "")
+            break
         }
     }
 }
+
+fun registrationAuthenticationInterface () {
+    while (true) {
+        println("""
+            Выберите одну из опций:
+            1. Зарегистрироваться
+            2. Войти в существующий аккаунт
+        """.trimIndent())
+        val userChoice = readln().toInt()
+        if (userChoice == 1) {
+            // registration
+        } else if (userChoice == 2) {
+            // authentication
+        }
+    }
+}
+
